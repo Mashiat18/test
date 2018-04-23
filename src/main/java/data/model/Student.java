4cbba9e4a792276ac38e94183893693d7cd878d5
mashi <mashiat18zahan@gmail.com>
2018-04-23 22:51:04 +0600

    all about tos

---
+++ src/main/java/data/model/Student.java
@@ -0,0 +1,27 @@
+package data.model;
+
+public class Student {
+    private Integer studentId;
+    private String studentName;
+
+    public Student() {
+    }
+
+    public Integer getStudentId() {
+        studentId = 1;
+        return studentId;
+    }
+
+    public void setStudentId(Integer studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        studentName = "dcj";
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+}
